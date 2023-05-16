e6c392662767f2b9029cb95e326f3afa54073be5
John Rees <reesj@nlm.nih.gov>
2023-05-16 10:16:34 -0400

    Delete public/assets/js directory

--- public/assets/js/nlm-scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-	var url = window.location.href;
-
-	if (url.indexOf("/resources/") !== -1 || url.indexOf("/top_containers/") !== -1 || url.indexOf("/archival_objects/") !== -1) {
-		document.getElementById("spec_request").style.display = "block";
-	}
-
-});
